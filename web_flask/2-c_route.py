8cf42a07b0f4166f756869119c789d67435ea589
bniyomukiz <b.niyomukiz@alustudent.com>
2024-02-24 15:08:11 +0000

    done

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-"""
-Starting a Flask app a simple one
-"""
+"""Starts a Flask web application.
 
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
+    /c/<text>: Displays 'C' followed by the value of <text>.
+"""
 from flask import Flask
-from markupsafe import escape
-
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def home():
-    """
-    home page return hello HBNB
-    """
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Displays 'Hello HBNB!'."""
     return "Hello HBNB!"
-
